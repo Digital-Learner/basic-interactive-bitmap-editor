fd3beb8080ab1b71f301c0717c3f7ae352d3a495
railyboy <s.m.github@gmail.com>
2014-05-23 12:30:35 +0100

    Quick refactor to DRY up test setup

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -5,19 +5,18 @@ describe 'Image' do
   before do
     @cols = 5
     @rows = 6
+    @bitmap = Image.new(@cols, @rows)
   end
 
   it "has 'M (cols)' x 'N (rows)' pixels" do
-    bitmap = Image.new(@cols,@rows)
-    expect(bitmap.cols).to eql(5)
-    expect(bitmap.rows).to eql(6)
+    expect(@bitmap.cols).to eql(5)
+    expect(@bitmap.rows).to eql(6)
   end
 
   it "has initial colour of 'O'" do
-    bitmap = Image.new(@cols, @rows)
     (1..@cols).map do |col|
       (1..@rows).map do |row|
-        colour = bitmap.get_pixel_colour([col,row])
+        colour = @bitmap.get_pixel_colour([col,row])
         expect("O").to eq(colour)
       end
     end
@@ -25,12 +24,11 @@ describe 'Image' do
 
   it "can set a pixels colour" do
     colour, pixel = "C", [1,1]
-    bitmap = Image.new(@cols,@rows)
-    bitmap.set_pixel_colour(pixel, colour)
-    expect(bitmap.get_pixel_colour(pixel)).to eq("C")
+    @bitmap.set_pixel_colour(pixel, colour)
+    expect(@bitmap.get_pixel_colour(pixel)).to eq("C")
   end
 
   it "returns a pixels colour" do
-    expect(Image.new(@cols,@rows).set_pixel_colour([3,4], "J")).to eq("J")
+    expect(@bitmap.set_pixel_colour([3,4], "J")).to eq("J")
   end
 end
