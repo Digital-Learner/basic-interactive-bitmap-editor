55e9883a8d776e4aaaf582c178b2aa8858db43b8
railyboy <s.m.github@gmail.com>
2014-05-28 17:23:07 +0100

    Fills a region based on neighbours and colours

--- lib/image.rb
+++ lib/image.rb
@@ -26,11 +26,12 @@ class Image
 
   def colour_horizontal_segment(from_col, to_col, row, colour)
     (from_col..to_col).each{|col| set_pixel_colour([row, col], colour)}
+  def fill(x, y, colour)
+    current_pixel_colour = get_pixel_colour(x, y)
+    flood_fill(x, y, current_pixel_colour, colour)
+    @pixels
   end
 
-  def get_neighbours(col,row)
-    perimeter = (row -1 .. row + 1).map{|row| (col - 1 .. col + 1).map {|col| [col,row]} }
-    perimeter.flatten(1) - [[col,row]]
   def get_neighbours(col, row)
     rows = (row - 1 .. row + 1).select{|row| (1..@rows).include?(row)}
     cols = (col - 1 .. col + 1).select{|col| (1..@cols).include?(col)}
@@ -39,5 +40,27 @@ class Image
     puts "\nmy neighbours(#{col},#{row}): #{neighbours}"
     return neighbours
   end
+
+  def flood_fill(x, y, target_colour, replacement_colour)
+    # puts "\nflood_fill(x=> #{x}, y=> #{y}, target_colour => #{target_colour}, replacement_colour => #{replacement_colour})"
+
+    # https://en.wikipedia.org/wiki/Flood_fill
+    # One implicitly stack-based (recursive) flood-fill implementation (for a two-dimensional array) goes as follows:
+
+    # Flood-fill (node, target-color, replacement-color):
+    #  1. If target-color is equal to replacement-color, return.
+    return if target_colour == replacement_colour
+    #  2. If the color of node is not equal to target-color, return.
+    return if get_pixel_colour(x, y) != target_colour
+    #  3. Set the color of node to replacement-color.
+    set_pixel_colour(x, y, replacement_colour)
+    #  4. Perform Flood-fill (one step to the west of node, target-color, replacement-color).
+    flood_fill(x - 1, y, target_colour, replacement_colour) if (1..@rows).cover?(x)
+    #     Perform Flood-fill (one step to the east of node, target-color, replacement-color).
+    flood_fill(x + 1, y, target_colour, replacement_colour) if (1..@rows).cover?(x)
+    #     Perform Flood-fill (one step to the north of node, target-color, replacement-color).
+    flood_fill(x, y - 1, target_colour, replacement_colour) if (1..@cols).cover?(y)
+    #     Perform Flood-fill (one step to the south of node, target-color, replacement-color).
+    flood_fill(x, y + 1, target_colour, replacement_colour) if (1..@cols).cover?(y)
   end
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -51,15 +51,37 @@ describe 'Image' do
     expect(@bitmap.show).to eq bitmap
   end
 
-  it "locates adjacent pixels" do
-    neighbours = [ [2,3], [3,3], [4,3], [2,4], [4,4], [2,5], [3,5], [4,5] ]
-    expect(@bitmap.get_neighbours(3,4)).to eq neighbours
+  # it "locates adjacent pixels" do
+  #   neighbours = [ [2,3], [3,3], [4,3], [2,4], [4,4], [2,5], [3,5], [4,5] ]
+  #   expect(@bitmap.get_neighbours(3,4)).to eq neighbours
 
-    neighbours = [ [4,5],[5,5],[4,6] ]
-    expect(@bitmap.get_neighbours(5,6)).to eq neighbours
+  #   neighbours = [ [4,5],[5,5],[4,6] ]
+  #   expect(@bitmap.get_neighbours(5,6)).to eq neighbours
 
-    neighbours = [ [2,2],[3,2],[4,2],[2,3],[4,3],[2,4],[3,4],[4,4] ]
-    expect(@bitmap.get_neighbours(3,3)).to eql neighbours
-  end
+  #   neighbours = [ [2,2],[3,2],[4,2],[2,3],[4,3],[2,4],[3,4],[4,4] ]
+  #   expect(@bitmap.get_neighbours(3,3)).to eql neighbours
+  # end
+
+  #   # 12345  => [     pixels                                  Array access
+  # # 1 JJJJJ      [[1,1,J],[2,1,J],[3,1,J],[4,1,J],[5,1,J]],            [0][0], [0][1], [0][2], [0][3], [0][4]
+  # # 2 JJZZJ      [[1,2,J],[2,2,J],[3,2,J],[4,2,J],[5,2,J]],
+  # # 3 JWJJJ      [[1,3,J],[2,3,W],[3,3,J],[4,3,J],[5,3,J]],
+  # # 4 JWJJJ      [[1,4,J],[2,4,W],[3,4,J],[4,4,J],[5,4,J]],
+  # # 5 JJJJJ      [[1,5,J],[2,5,J],[3,5,J],[4,5,J],[5,5,J]],
+  # # 6 JJJJJ      [[1,6,J],[2,6,J],[3,6,J],[4,6,J],[5,6,J]]             [5][0], [5][1], [5][2], [5][3], [5][4]
+  # #             ]
+
+  it "fills region 'R' with colour 'C'" do
+    bitmap = "J J J J J\nJ J J J J\nJ A J J J\nJ J J J J\nJ J J J J\nJ J J J J\n"
+    @bitmap.set_pixel_colour(2,3,"A")
+    @bitmap.fill(3,3,"J")
+    expect(@bitmap.show).to eq bitmap
+
+    bitmap = "J J J J J\nJ J Z Z J\nJ W J J J\nJ W J J J\nJ J J J J\nJ J J J J\n"
+    @bitmap.set_pixel_colour(2,3,"A")
+    @bitmap.colour_vertical_segment(2,3,4,"W")
+    @bitmap.colour_horizontal_segment(3,4,2,"Z")
+    @bitmap.fill(3,3,"J")
+    expect(@bitmap.show).to eq bitmap
   end
 end
